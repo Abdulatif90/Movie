1c0367ac788c5fa45c041d6d7e89a29e4c03bf3a
Abdulatif <abdulatifsh90@gmail.com>
2025-05-11 11:58:55 +0900

    feat: learning with state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,118 @@
-import './app.css'
-import AppInfo from "../app-info/app-info";
-import SearchPanel from "../search-panel/search-panel";
-import AppFilter from '../app-filter/app-filter';
-import MovieList from '../movie-list/movie-list';
-import MovieAddFrom from '../movie-add-form/movie-add-form';
+import { Component } from "react"
+// import './app.css'
+// import AppInfo from "../app-info/app-info";
+// import SearchPanel from "../search-panel/search-panel";
+// import AppFilter from '../app-filter/app-filter';
+// import MovieList from '../movie-list/movie-list';
+// import MovieAddFrom from '../movie-add-form/movie-add-form';
 
 
 
+class User extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            counter:0,
+        }
+    }
+   
+    // State o`zini mutation qilib bo`lmaydi. o`rniga setStatedan foydalanish kerak
+    // clickhandler = () =>{
+    //     this.setState({
+    //         counter:this.state.counter + 1
+    //     }) 
+    // }   // bu setState har bir o`zgarishi uchun State ga borib + 1 qo`shadi bu esa Statega bog`langanligi uchn sekinro ishlaydi
+   
+     clickHandlerUp = () =>{
+        this.setState(prevState => ({
+            counter: prevState.counter + 1
+        })
+    )}
+   
+clickHandlerDown = () => {
+    this.setState(prevState => {
+        if (prevState.counter > 0) {
+            return { counter: prevState.counter - 1 };
+        } else {
+            return null; // hech narsa o'zgartirilmaydi yani state ishlamaydi.
+        }
+    });
+}
+   
+     clickHandlerReset = () =>{
+        this.setState(prevState => ({
+            counter: prevState.counter = 0
+        }) 
+    )}
+   
+
+  render () {
+  const {firstname, lastname, link } = this.props   
+  
+
+  return (
+    <div  className="w-50 mx-auto">
+      <div className="border p-3 mt-5">
+        <h4>
+            Mening ismim - {firstname}, familiyam - {lastname}
+        </h4>
+        <a href={link}>Youtube kanalim</a>
+        <div className="mt-3">
+            <button onClick={this.clickHandlerUp} className="btn btn-success">
+            increment
+            </button>
+            <button onClick={this.clickHandlerDown} className="btn btn-danger mx-2" >
+            decrement
+            </button>
+            <button onClick={this.clickHandlerReset} className="btn btn-info">
+            reset
+            </button>
+            <p className="text-center">
+                {this.state.counter}
+            </p>
+        </div>
+      </div>
+    </div>
+  )
+} 
+}
 
 const App = () => {
-    const data = [
-        {name : "Shaytanat", viewers : 989},
-        {name : "Jaloliddin", viewers : 787},
-        {name : "Osmondagi bolalar", viewers : 784},
-    ]
-   
     return (
-        <div className='app font-monospace'>
-           <div className='content'>
-           <AppInfo/>
-                <div className='search-panel'>
-                    <SearchPanel/>
-                    <AppFilter/>    
-                </div>
-               <MovieList data = {data}/>
-                <MovieAddFrom/>       
-                 
+        <div>  
+            <div> 
+            <User firstname = "Abdulatif" lastname = "Sharipov" link = "youtube.com" />
+            <User firstname = "Shaxzodbek" lastname = "Boymatov" link = "youtube.com" />
             </div>
-         </div>
-    );
-    }
+        </div>
+
+    )
+}
+
+
+
+
+// const App = () => {
+//     const data = [
+//         {name : "Shaytanat", viewers : 989, id: 1},
+//         {name : "Jaloliddin", viewers : 787, id: 2},
+//         {name : "Osmondagi bolalar", viewers : 784, id: 3},
+//     ]
+   
+//     return (
+//         <div className='app font-monospace'>
+//            <div className='content'>
+//            <AppInfo/>
+//                 <div className='search-panel'>
+//                     <SearchPanel/>
+//                     <AppFilter/>    
+//                 </div>
+//                <MovieList data = {data}/>
+//                 <MovieAddFrom/>       
+                 
+//             </div>
+//          </div>
+//     );
+//     }
 
     export default App
--- src/components/movie-list/movie-list.js
+++ src/components/movie-list/movie-list.js
@@ -1,11 +1,12 @@
 import './movie-list.css'
 import MovieListItem from '../movie-list-item/movie-list-item'
 
+
 const MovieList = ({data}) => {
   return (
     <ul className="movie-list">
       {data.map(item => (
-        <MovieListItem {...item}/>
+        <MovieListItem key = {item.id}  name = {item.name} viewers = {item.viewers}/>
       ))}
     </ul>
   )
